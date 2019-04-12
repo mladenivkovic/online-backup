9ca0ac251c6e519d68a1e86564490ed33582e825
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-04-12 13:27:06 +0200

    added print_extra_particle_data

---
+++ online-backup/meshless/print_extra_particle_data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# print extra added particle's (with ID=101) data
+
+
+
+
+import h5py
+import os
+import sys
+import numpy
+
+if len(sys.argv) < 2:
+    print("Need hdf5 file")
+    quit()
+
+
+fname = sys.argv[1]
+
+f = h5py.File(fname)
+
+grp = f['PartType0']
+coords = grp['Coordinates'][:]
+ids = grp['ParticleIDs'][:]
+
+index = numpy.where(ids==101)[0]
+print(index)
+print(coords[index])
+
